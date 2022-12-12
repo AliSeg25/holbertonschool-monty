34bfad68dad49d26f9e7b937a1dfb1b1fcf31dcd
Your Name <as.segueg2501@gmail.com>
2022-12-12 02:20:58 -0800

    add new function the delete the top element

---
+++ pop.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+/**
+ * pint - Prints the top of the stack.
+ * @head: The head of the dlistint_t list.
+ * @n: the ligne
+ */
+void pop(stack_t **head, unsigned int ligne)
+{
+	stack_t *tmp = *head;
+
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack", ligne);
+		return;
+	}
+
+	*head = (*head)->next;
+	free(tmp);
+}
