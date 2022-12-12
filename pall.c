aa638f86d3a62223aff35cd39f0d3a776a1ec8fb
Your Name <as.segueg2501@gmail.com>
2022-12-12 01:13:35 -0800

    add print

---
+++ pall.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * pall - Prints all the elements stack.
+ * @h: The head of the dlistint_t list.
+ * @n: no attribute
+ */
+void pall(stack_t *h, __attribute((unused))unsigned int n)
+{
+	stack_t *temp = *h;
+
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+}
